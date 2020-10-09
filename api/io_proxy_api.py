82cd49fca854b8f78bffe2f444e6a1259d17cf95
Mustafa <mustafalani@gmail.com>
2020-10-09 13:25:43 +0200

    add requirements.txt

--- api/io_proxy_api.py
+++ api/io_proxy_api.py
@@ -25,6 +25,7 @@ app = Flask(__name__)
 with open(r'conf.yml') as configfile:
     configuration = yaml.load(configfile, Loader=yaml.FullLoader)
     api_proxy_url = configuration['api_proxy_server']['api_proxy_url']
+    #/v2/servers/_defaultServer_/vhosts/_defaultVHost_/
     serverName = configuration['api_proxy_server']['server_name']
     apiUrl = configuration['publisher-engine']['api_url']
     apiPort = str(configuration['publisher-engine']['port'])
@@ -52,7 +53,7 @@ def getServerConfig():
 # ----------------------
 # Applications
 # ----------------------
-# Applications : Retrieves the list of Applications for the specifed server
+# Applications - Retrieve all apps: Retrieves the list of Applications for the specifed server
 # GET: get /v2/servers/{serverName}/vhosts/{vhostName}/applications
 @app.route(api_proxy_url + 'applications', methods=['GET'])
 def getApplications():
@@ -81,36 +82,41 @@ def getApplications():
     return Response(xml_out, mimetype='text/xml')
 
 
-# Applications : Retrieves the specified Application configuration
+# Applications - Retrieve app: Retrieves the specified Application configuration
 # GET: get /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}
 @app.route(api_proxy_url + 'applications/<name>', methods=['GET'])
 def getApplication(name):
-    response = requests.get(apiUrl + ':' + apiPort + '/applications/?name=' + name)
-    root = ET.fromstring(response.content)
-    import xml.etree.ElementTree as etree
-    xml_root = etree.Element('Application')
-    xml_root.set('serverName', serverName)
-    for app in root.findall('*'):
-        app_name = app.find('name')
-        app_description = app.find('description')
-        app_type = app.find('type')
-        xml_item_name = etree.Element('Name')
-        xml_item_name.text = app_name.text
-        xml_root.append(xml_item_name)
-        xml_item_type = etree.Element('AppType')
-        xml_item_type.text = app_type.text
-        xml_root.append(xml_item_type)
-        xml_item_description = etree.Element('Description')
-        xml_item_description.text = app_description.text
-        xml_root.append(xml_item_description)
-    doc_type = '<?xml version="1.0" encoding="UTF-8" ?>'
-    _tostring = etree.tostring(xml_root).decode('utf-8')
-    xml_out = (f"'{doc_type}{_tostring}'").replace('\'', '')
-    return Response(xml_out, mimetype='text/xml')
+    response = requests.get(apiUrl + ':' + apiPort + '/applications/' + name)
+    if response.status_code == 404:
+        xml_out = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><error><code>404</code><message>The server has not found anything matching the request URI</message></error>"
+        return Response(xml_out, status=404,  mimetype='text/xml')
+    else:
+        response = requests.get(apiUrl + ':' + apiPort + '/applications/?name=' + name)
+        root = ET.fromstring(response.content)
+        import xml.etree.ElementTree as etree
+        xml_root = etree.Element('Application')
+        xml_root.set('serverName', serverName)
+        for app in root.findall('*'):
+            app_name = app.find('name')
+            app_description = app.find('description')
+            app_type = app.find('type')
+            xml_item_name = etree.Element('Name')
+            xml_item_name.text = app_name.text
+            xml_root.append(xml_item_name)
+            xml_item_type = etree.Element('AppType')
+            xml_item_type.text = app_type.text
+            xml_root.append(xml_item_type)
+            xml_item_description = etree.Element('Description')
+            xml_item_description.text = app_description.text
+            xml_root.append(xml_item_description)
+        doc_type = '<?xml version="1.0" encoding="UTF-8" ?>'
+        _tostring = etree.tostring(xml_root).decode('utf-8')
+        xml_out = (f"'{doc_type}{_tostring}'").replace('\'', '')
+        return Response(xml_out, mimetype='text/xml')
 
 
-# Applications : Adds an Application to the list of Applications for the specifed vhost
-# post /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}
+# Applications - Add App: Adds an Application to the list of Applications for the specifed vhost
+# POST: /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}
 @app.route(api_proxy_url + 'applications/<name>', methods=['POST'])
 def addApplication(name):
     if not request.json or not 'name' in request.json:
@@ -144,18 +150,15 @@ def addApplication(name):
         return Response(r.text, mimetype='text/xml'), 404
 
 
-# Applications : Adds the specified advanced Application configuration
-# post /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}
+# Applications - Add app adv config: Adds the specified advanced Application configuration
+# POST: /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}/adv
 @app.route(api_proxy_url + 'applications/<name>/adv', methods=['POST'])
 def addApplicationAdv(name):
     r = name
     return (r, 200)
-    #if not request.json or not 'name' in request.json:
-    #    abort(400)
-    #app_name = request.json['name']
 
-# Applications : Adds an Application to the list of Applications for the specifed vhost
-# post /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}
+# Applications - Delete App: Delete an Application from the list of Applications for the specifed vhost
+# DEL: /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}
 @app.route(api_proxy_url + 'applications/<name>', methods=['DELETE'])
 def deleteApplication(name):
     app_name = name
@@ -167,6 +170,22 @@ def deleteApplication(name):
         return Response(r.text, mimetype='text/xml'), 404
     else:
         return Response(r.text, mimetype='text/xml'), 400
+
+
+# Applications - Update App: Update an Application in the list of Applications for the specifed vhost
+# PUT: /v2/servers/{serverName}/vhosts/{vhostName}/applications/{appName}
+
+
+# ----------------------
+# Server
+# ----------------------
+# Server - restart nginx status
+# PUT: v2/servers/_defaultServer_/actions/restart
+@app.route('/v2/servers/_defaultServer_/actions/restart', methods=['PUT'])
+def restart():
+    restart_cmd = "ls"
+    output = subprocess.Popen(restart_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    print(output.returncode)
 
 if __name__ == '__main__':
     app.run(debug=False)
